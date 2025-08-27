51b31de7d97be08825c7db8ae068bf7cf04cdc57
Yefimchuk08 <dmytroyefimchuk08@gmail.com>
2025-08-27 17:37:02 +0300

    Add project files.

---
+++ WebApplication1/Controllers/ArtistController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class ArtistController : Controller
+    {
+        public IActionResult Artist()
+        {
+            return View();
+        }
+        public IActionResult More()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebApplication1/Models/Car.cs
@@ -0,0 +1,12 @@
+﻿namespace WebApplication3.Models
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public string Model { get; set; }
+        public string Color { get; set; }
+        public int Year { get; set; }
+        public string Type { get; set; }
+        public int Price { get; set; }
+    }
+}
